c872f694a5b0847ba92d93a0c4f542efcaa29799
bulat <bulatxajrutdinoff@yandex.ru>
2022-11-12 17:03:06 +0300

    counter in options api design

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,32 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <div>
+      <button class="btn" @click="decreaseCounter">-</button>
+      <span class="counter">{{ counter }}</span>
+      <button class="btn" @click="increaseCounter">+</button>
+    </div>
   </div>
 </template>
+
+
+<style>
+
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    decreaseCounter() {
+      this.counter -= 1;
+    },
+    increaseCounter() {
+      this.counter += 1;
+    },
+  }
+}
+</script>
