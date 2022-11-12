756f17e7aae2afddbf898ffa25bd7a2f18baea75
bulat <bulatxajrutdinoff@yandex.ru>
2022-11-12 20:17:36 +0300

    slots

---
+++ src/components/Modal.vue
@@ -0,0 +1,32 @@
+<template>
+
+  <teleport to=".modals-container">
+    <div class="modal">
+
+      <h2><slot name="title"></slot></h2>
+      <slot></slot>
+      <button @click="false">Hide modal</button>
+    </div>
+  </teleport>
+</template>
+
+<script setup>
+import { useSlots } from "vue"
+
+const slots = useSlots()
+console.log(slots.title())
+</script>
+
+<style scoped>
+.modal {
+  background-color: beige;
+  padding: 10px;
+  color: #000;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+}
+</style>
--- src/views/ModalsView.vue
+++ src/views/ModalsView.vue
@@ -3,36 +3,25 @@
     <h1>Modals</h1>
 
     <button @click="showModal">Show modal</button>
-
-    <teleport to=".modals-container">
-      <div class="modal" v-show="isShown">
-        <h2>This is a modal</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, praesentium.</p>
-        <button @click="hideModal">Hide modal</button>
-      </div>
-    </teleport>
+    <Modal v-if="isShown">
+      <template #title>
+        This is a modal!
+      </template>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, praesentium.</p>
+    </Modal>
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+import Modal from '@/components/Modal.vue'
+
 const isShown = ref(false)
 
 const hideModal = () => isShown.value = false
-
 const showModal = () => isShown.value = true
 </script>
 
 <style scoped>
-.modal {
-  background-color: beige;
-  padding: 10px;
-  color: #000;
-  position: absolute;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  z-index: 1;
-}
+
 </style>
