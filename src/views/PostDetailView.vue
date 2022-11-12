370f4db7eedb876289c00eccedfd97ea8b71498b
bulat <bulatxajrutdinoff@yandex.ru>
2022-11-12 19:48:49 +0300

    useRoute and useRouter

---
+++ src/views/PostDetailView.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="about">
+    <h1>This is a post detail</h1>
+    <p>Post with id {{ $route.params.id }}</p>
+    <div>
+      <button @click="showPostId">Show Post Id</button>
+    </div>
+    <div>
+      <button @click="goHomeIn3Seconds">Go home in 3 seconds</button>
+    </div>
+    <div>
+      <button @click="goToSecondPost">Go to second post</button>
+    </div>
+    <div>
+      <RouterLink to="/posts">&lt; Back</RouterLink>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useRoute, useRouter } from 'vue-router'
+
+const route = useRoute(),
+    router = useRouter()
+
+const showPostId = async () => {
+  console.log(
+      route.params.id
+  )
+}
+
+const goHomeIn3Seconds = () => {
+  setTimeout(() => {
+    // router.push('/')
+    router.push({ name: 'home' })
+  }, 3000)
+}
+
+const goToSecondPost = () => {
+    router.push({ name: 'postDetail', params: { id: 2 } })
+}
+</script>
---
+++ src/views/PostsView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="about">
+    <h1>Posts</h1>
+    <ul>
+      <li>
+        <RouterLink to="/postDetail/1">Post 1</RouterLink>
+      </li>
+      <li>
+        <RouterLink to="/postDetail/2">Post 2</RouterLink>
+      </li>
+    </ul>
+  </div>
+</template>
