6dfb994887809e1f308c4caf0c4215dfa8d3402f
bulat <bulatxajrutdinoff@yandex.ru>
2022-11-12 21:03:51 +0300

    dynamic components

---
+++ src/components/DarkModal.vue
@@ -0,0 +1,47 @@
+<template>
+
+  <teleport to=".modals-container">
+    <div class="dark-modal" v-if="modelValue">
+      <h2>{{ title }}</h2>
+      <slot></slot>
+      <button @click="hideModal">Hide modal</button>
+    </div>
+  </teleport>
+</template>
+
+<script setup>
+import {defineEmits} from "vue";
+
+const props = defineProps({
+  title: {
+    type: String,
+    default: 'Title'
+  },
+  modelValue: {
+    type: Boolean,
+    default: false,
+  }
+});
+
+console.log(props.title)
+
+const emit = defineEmits(['update:modelValue'])
+
+const hideModal = () => {
+  emit('update:modelValue', false)
+}
+</script>
+
+<style scoped>
+.dark-modal {
+  background-color: darkslategray;
+  padding: 10px;
+  color: #000;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+}
+</style>
--- src/views/ModalsView.vue
+++ src/views/ModalsView.vue
@@ -1,24 +1,44 @@
 <template>
   <div class="modals">
     <h1>Modals</h1>
-
+    <div>
+      <label v-for="(comp, key) of modalComponents" style="display: block">
+        {{key}} <input type="radio" v-model="modalComponentTheme" :value="key">
+      </label>
+    </div>
     <button @click="showModal">Show modal</button>
-    <Modal
+    <component
+        :is="modalComponent"
         v-model="isShown"
         title="This is a title (from prop)"
     >
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, praesentium.</p>
-    </Modal>
+    </component>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import Modal from '@/components/Modal.vue'
+import {ref, shallowRef, watch} from 'vue'
+import WhiteModal from '@/components/WhiteModal.vue'
+import DarkModal from '@/components/DarkModal.vue'
+
+const modalComponents = {
+  WhiteModal,
+  DarkModal
+}
 
 const isShown = ref(false)
 
+const modalComponentTheme = ref('WhiteModal')
+const modalComponent = shallowRef(modalComponents[modalComponentTheme])
+
 const showModal = () => isShown.value = true
+
+watch(modalComponentTheme, (modalComponentTheme) => {
+  modalComponent.value = modalComponents[modalComponentTheme]
+})
+
+
 </script>
 
 <style scoped>
